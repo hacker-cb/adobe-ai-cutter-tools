c5ec3bd67bbde52df63f4638cbdc35ec324e15ed
Pavel Sokolov <pavel@tvip.ru>
2023-03-24 11:30:19 +0300

    fix fix windows_debug_mode.cmd

--- install/windows_debug_mode.cmd
+++ install/windows_debug_mode.cmd
@@ -1,5 +1,6 @@
 
-REG ADD HKCU\Software\Adobe\CSXS.8 /v /f PlayerDebugMode /t REG_SZ /d 1
-REG ADD HKCU\Software\Adobe\CSXS.9 /v /f PlayerDebugMode /t REG_SZ /d 1
-REG ADD HKCU\Software\Adobe\CSXS.10 /v /f PlayerDebugMode /t REG_SZ /d 1
-REG ADD HKCU\Software\Adobe\CSXS.11 /v /f PlayerDebugMode /t REG_SZ /d 1
+REG ADD HKCU\Software\Adobe\CSXS.8 /f /v PlayerDebugMode /t REG_SZ /d 1
+REG ADD HKCU\Software\Adobe\CSXS.9 /f /v PlayerDebugMode /t REG_SZ /d 1
+REG ADD HKCU\Software\Adobe\CSXS.10 /f /v PlayerDebugMode /t REG_SZ /d 1
+REG ADD HKCU\Software\Adobe\CSXS.11 /f /v PlayerDebugMode /t REG_SZ /d 1
+
